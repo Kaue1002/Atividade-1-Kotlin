8700e54eb6e8705ef59e17d7a18f326af05d4944
Kaue1002 <kaue100204@edu.unipar.br>
2024-08-19 19:35:30 -0300

    Update Atividade 3.kt

--- EXERCICIOS/src/Atividade 3.kt
+++ EXERCICIOS/src/Atividade 3.kt
@@ -1,17 +1,38 @@
-fun main(args: Array<String>) {
+fun main() {
+    val nomeProduto: String = "Teclado";
+    val precoUnitario: Double = 2500.0;
+    val quantidadeComprada: Int = 3;
+    val taxaImposto: Double = 0.02;
+    val margemLucro: Double = 0.2;
 
-    var nomeprod: String = "Teclado"
-    var preçounit: Int = 120
-    var quantprod: Int = 2
-    var taxaimposto: Int = 3%
-    var margemlucro: Int = 20%
+    val valorTotalSemImpostos: Double = precoUnitario * quantidadeComprada;
+    val valorImposto: Double = valorTotalSemImpostos * (taxaImposto / 100);
+    val valorTotalComImpostos: Double = valorTotalSemImpostos + valorImposto;
+    val precoVenda: Double = valorTotalComImpostos * (1 + margemLucro / 100);
 
+    val decimalFormat = DecimalFormat("R$ #,##0.00");
+    val valorTotalComImpostosFormatado = decimalFormat.format(valorTotalComImpostos);
+    val precoVendaFormatado = decimalFormat.format(precoVenda);
 
-    var totalsemimposto: Int = preçounit * quantprod
-    var
+    val mensagemFinal = "Produto: $nomeProduto\n" + "Valor Total com Impostos: $valorTotalComImpostosFormatado\n" + "Preço de Venda Sugerido: $precoVendaFormatado";
 
-            
+    println("Nome do Produto: $nomeProduto");
 
+    println("Preço Unitário: R$${"%.2f".format(precoUnitario)}");
 
+    println("Quantidade Comprada: $quantidadeComprada");
 
+    println("Taxa de Imposto: $taxaImposto%");
+
+    println("Margem de Lucro: $margemLucro%");
+
+    println("Valor Total Sem Impostos: R$${"%.2f".format(valorTotalSemImpostos)}");
+
+    println("Valor do Imposto: R$${"%.2f".format(valorImposto)}");
+
+    println("Valor Total com Impostos: $valorTotalComImpostosFormatado");
+
+    println("Preço de Venda Sugerido: $precoVendaFormatado");
+
+    println(mensagemFinal);
 }
